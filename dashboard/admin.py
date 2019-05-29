92931ce03677783ead7e2d1a6be2e767055747dd
YukinoKohmoto <yukino@Ys.local>
2019-05-29 22:27:30 +0900

    cleaned up the models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,25 +1,7 @@
 from django.contrib import admin
-from .models import Choice, Question, Issue, Task
+from .models import Issue, Task
 from django.forms import TextInput, Textarea
 from django.db import models
-
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    # fields = ['question_text', 'pub_date']
-    fieldsets = [
-        (None, {'fields':['question_text']}),
-        ('Date information', {'fields': ['pub_date'],
-                              'classes':['collapse']}),
-    ]
-    inlines = [ChoiceInline] 
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
 
 
 class TaskInline(admin.TabularInline):
@@ -66,7 +48,5 @@ class TaskAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Task, TaskAdmin)
