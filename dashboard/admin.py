2be9505f5c2d7dac097ae3b2cfc29119a95934cb
YukinoKohmoto <yukino@Ys.local>
2019-05-31 00:07:20 +0900

    inter-links between issue/tasks

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import Issue, Task
 from django.forms import TextInput, Textarea
 from django.db import models
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 class TaskInline(admin.TabularInline):
     model = Task
-    fields = ['task_title', 'status', 'priority']
+    fields = ['status', 'priority']
     ordering = ('priority',)
+    show_change_link = True
 
 
 class IssueAdmin(admin.ModelAdmin):
@@ -30,9 +33,14 @@ class IssueAdmin(admin.ModelAdmin):
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('task_title', 'status', 'priority')
+    def link_to_Issue(self, obj):
+        link=reverse("admin:dashboard_issue_change", args=[obj.issue_id]) 
+        #model name has to be lowercase
+        return mark_safe(u'<a href="%s">%s</a>' % (link, obj.issue))
+
+    list_display = ('task_title', 'link_to_Issue', 'status', 'priority')
     ordering = ('priority',)
-    list_filter = ['status']
+    list_filter = ['status', 'issue']
     # fold the sub info in detail pate
     fieldsets = [
         ('Status information', {'fields':['priority',
@@ -44,7 +52,6 @@ class TaskAdmin(admin.ModelAdmin):
         models.CharField: {'widget': TextInput(attrs={'size':'20'})},
         models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
     }
-
 
 
 # Register your models here.
